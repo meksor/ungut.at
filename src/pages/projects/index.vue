25a6dfaa0963bdf754293f39531d1e6c0c3b80d0
Max Wolschlager <rxtx@ungut.at>
2024-08-30 20:55:51 +0200

    fix project index

--- src/pages/projects/index.vue
+++ src/pages/projects/index.vue
@@ -1,5 +1,11 @@
 <template>
   <div>
-    <ContentDoc />
+    <ContentDoc path="/projects/" />
+    <ContentList path="/projects" v-slot="{ list }">
+      <div v-for="p in list" :key="p._path">
+        <a :href="p._path"> <h2>{{ p.title }}</h2> </a>
+        <p>{{ p.date }}</p>
+      </div>
+    </ContentList>
   </div>
 </template>
