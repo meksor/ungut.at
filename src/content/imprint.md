8e065380befdd999e23d9e5de0d8f5e8b6b642c3
Max Wolschlager <rxtx@ungut.at>
2025-01-18 16:32:01 +0100

    use html instead of nuxt-content

--- src/content/imprint.md
+++ src/content/imprint.md
@@ -1,7 +1,3 @@
----
-title: Max Wolschlager
----
-
 **Max Wolschlager**  
 Programmer, Artist
 ::br
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -36,13 +36,27 @@
   <row class="my-4 container" justify-content="middle">
     <card>
       <card-text>
-        <ContentQuery path="/legal" find="one">
+                
+        The content on this website may
+        be copyrighted. Get in touch if 
+        you want to use it
+
+        <br>
+
+        This website contains links to other websites
+        with content for which I am not responsible.
+        § 17 ECG clears liability, as I have no knowledge
+        of illegal activity on these sites and would 
+        remove links to sites indulging in such if it 
+        were to come to my attention.
+
+        <!--ContentQuery path="/legal" find="one">
           <template #default="{ data: lData }">
             <ContentRenderer class="ta-end of-visible" :value="lData" />
           </template>
           <template #empty></template>
           <template #not-found></template>
-        </ContentQuery>
+        </ContentQuery-->
       </card-text>
     </card>
 </row>
